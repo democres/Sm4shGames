6914ac374a467c37fc5567805aade70441b7615c
David Figueroa <davidf.naposystems@gmail.com>
2018-12-27 17:57:11 -0500

    add Launch Screen and HomeViewController

---
+++ Sm4shGames/Controllers/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Sm4shGames
+//
+//  Created by MacNapo on 12/27/18.
+//  Copyright © 2018 DavidF. All rights reserved.
+//
+
+import Foundation
--- Sm4shGames/SupportFiles/ViewController.swift
+++ Sm4shGames/SupportFiles/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func getStaredBtn(_ sender: Any) {
+        var z = 1
+        
+    }
+    
 }
 
