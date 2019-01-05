128ff899dddcd8cf7c8520588df68388bd41d4e7
Creativo Grafica <creativocalidadgrafica@macbook-pro-de-creativo.local>
2019-01-05 09:18:37 -0500

    Create Game model

---
+++ Sm4shGames/Models/Game.swift
@@ -0,0 +1,40 @@
+//
+//  Game.swift
+//  Sm4shGames
+//
+//  Created by Creativo Calidad Grafica on 5/01/19.
+//  Copyright © 2019 DavidF. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    let objectId: String
+    let name: String
+    var brand: String
+    var price: String
+    var imageUrl: String
+    var genre: String
+    var popular: Bool
+//    var rating: String
+//    var downloads: String
+//    var description: String
+//    var SKU: String
+    var createdAt: Date
+    var updatedAt: Date
+    
+    
+    init(objectId: String, name: String, brand: String, price: String, imageUrl: String, genre: String, popular: Bool, updatedAt: Date, createdAt: Date) {
+        self.objectId = objectId
+        self.name = name
+        self.brand = brand
+        self.price = price
+        self.imageUrl = imageUrl
+        self.genre = genre
+        self.popular = popular
+        self.updatedAt = updatedAt
+        self.createdAt = createdAt
+    }
+    
+}
